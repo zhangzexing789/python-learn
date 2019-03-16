7f059083fd193f60903c0427ce54b4a59f27c684
zhangzexing789 <1010761538@qq.com>
2019-03-16 23:27:33 +0800

    add division demo

---
+++ base/division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# @Time   : 2019/3/16 23:10
+# @Author : Joson
+# @File   : division.py
+print('10除以3：', 10 / 3)
+print('10.0除以3：', 10.0 / 3)
+print('10除以3，取整数：', 10 // 3)
+print('10除以3，取余数：', 10 % 3)
+
+
